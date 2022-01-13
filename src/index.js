b05c68a2bc5ecef0b78f3626d077e584b6976a37
ilyashityk <ilya350000@gmail.com>
2022-01-14 00:12:59 +0300

    feat: add number before 20 function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,57 @@
 module.exports = function toReadable (number) {
+    const strNumber = number.toString();
+    const lastChar = parseInt(strNumber[strNumber.length - 1]);
+    const penultimateChar = parseInt(strNumber[strNumber.length - 2]);
+    const lastTwoChars = parseInt(strNumber[strNumber.length - 2] + strNumber[strNumber.length - 1]);
+    const beforeTen = () => {
+      switch (lastChar) {
+        case 1:
+          return 'one';
+        case 2:
+          return 'two';
+        case 3:
+          return 'three';
+        case 4:
+          return 'four';
+        case 5:
+          return 'five';
+        case 6:
+          return 'six';
+        case 7:
+          return 'seven';
+        case 8:
+          return 'eight';
+        case 9:
+          return 'nine';
+      }
+    }
+    const beforeHundred = () => {
+      switch (lastTwoChars) {
+        case 11:
+          return 'eleven';
+        case 12:
+          return 'twelve';
+        case 13:
+          return 'thirteen';
+        case 14:
+          return 'fourteen';
+        case 15:
+          return 'fifteen';
+        case 16:
+          return 'sixteen';
+        case 17:
+          return 'seventeen';
+        case 18:
+          return 'eighteen';
+        case 19:
+          return 'nineteen';
+      }
+    }
+    const beforeThousand = () => {
   
-}
+    }
+    if (number === 0) return 'zero';
+    if (number < 10) return beforeTen();
+    if (number < 100) return beforeHundred();
+    if (number < 1000) return beforeThousand();
+  }
